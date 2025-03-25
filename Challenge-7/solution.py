cc11e406c812b2c01601e0ba7d37e3887aafad61
Rashad M <82885442+rasharashe@users.noreply.github.com>
2025-03-25 16:22:14 +0530

    Create solution.py for challenge-7

---
+++ Challenge-7/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxsum = nums[0]
+        cursum = nums[0]
+        for i in range(1, len(nums)):
+            cursum = max(cursum + nums[i], nums[i])
+            maxsum = max(maxsum, cursum)
+        return maxsum
